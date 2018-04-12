a06a27c0f0bfa98a3484694ced484d7e650f988e
Deepak Nagar <31900445+deepak525@users.noreply.github.com>
2018-04-12 13:26:29 +0530

    Update lottery.c

--- lottery.c
+++ lottery.c
@@ -4,163 +4,91 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
 void main()
 {
-	char process[5] = { 'A', 'B', 'C', 'D', 'E'},temp[10];
-	int arrival[10];
-	int brust[10];
-	int ticket[10][10];
-	int lottery[10] = {0,0,0,0,0,0,0,0,0,0};
-	int i,j,z,p=0,max_tick,q=0,pss=0;
-	
-	//Generate arrival time for each process randomly
-	for(i=0;i<5;i++)
+	int n,i,j,k,temp=65,flag=0;
+	char process[20];
+	int brust[20],priority[20],pos;
+	int time=0,quantom=1,tbt=0;
+	int z=0,lottery[20],ticket[20][20];
+	printf("Enter Number Of Process: ");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
 	{
-		int num = (rand()%5)+ 1;
-		arrival[i] = num;
+		process[i] = temp;
+		temp+=1;
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("\nEnter The Brust Time For Process %c: ",process[i]);
+		scanf("%d",&brust[i]);
+		printf("Enter The Priority For Process %c(b/w 1 to %d): ",process[i],n);
+		scanf("%d",&priority[i]);
 	}
 	
-	//Generate brust time for each process randomly
-	for(j=0;j<5;j++)
-	{
-		int num = (rand()%5) + 1;                     //random number b/w {1,15}
-		brust[j] = num;
-	}
-	
-	printf("Process   Arrival     Brust ");
-	
-	for(i=0;i<5;i++)
-	{
-		printf("\n  %c \t    %d    \t%d",process[i],arrival[i],brust[i]);
-	}
-	
-	printf("\n\n:::::   Quantom time is 1 Sec   ::::: \n\n");
-	
-	int time = 0,quantom=1;
-	
-    //arrange them on the bases of there arrival time
-	for (i= 0;i<5;i++)
-	{
-		for(j=0;j<4;j++) 
-		{
-			if (arrival[j] >= arrival[j + 1])
-			{
-				int temp2 = arrival[j];
-                arrival[j] = arrival[j + 1];
-                arrival[j + 1] = temp2;
-
-                char temp1 = process[j];
-                process[j] = process[j + 1];
-                process[j + 1] = temp1;
-			}
-		}	
-	}	
-	
-	printf("Process   Arrival     Brust ");
-	
-	for(i=0;i<5;i++)
-	{
-		printf("\n  %c \t    %d    \t%d",process[i],arrival[i],brust[i]);
-	}
-                 
-   while((brust[0]+brust[1]+brust[2]+brust[3]+brust[4])>0){
-   
-   	printf("\n\n\nBrust  process    Lottery \t Tickets");
-    p =0;
-    max_tick = 0;
-//assign one or more lottery numbers to each process
-	for(i=0;i<5;i++)
-	{
-		if(brust[i]>0)
-		{
-			lottery[i]=brust[i]/2;
-			if ((lottery[i] == 0) && (brust[i] > 0))
-                lottery[i] = 1;
-               
-			for (z = 0; z < lottery[i]; z++) {
-                    ticket[i][z] = p++;
-                        max_tick = p;
-                    }
-                    temp[i] = process[i];   
-					
-			printf("\n  %d\t   %c\t\t%d",brust[i],temp[i],lottery[i]);
-			for(z=0;z<lottery[i];z++)
-			{
-				printf("\t::%d::",ticket[i][z]);
-			   }   
-		}
-		
-		else if ((brust[i] > 0) && (time < arrival[i]))
-		{
-			temp[i] = process[p];
-            lottery[i] = brust[i] / 2;
-            if ((lottery[i] == 0) && (brust[i] > 0))
-                lottery[i] = 1;
-                
-            for (z = 0; z < lottery[i]; z++){
-                ticket[i][z] = p++;
-                max_tick=p;
-            }
-            
-            if (brust[i] > 0) {
-                time += quantom;
-                } else {
-                    time += (brust[i] + quantom);
-                }
-                
-            printf("\n  %d\t   %c\t\t%d",brust[i],temp[i],lottery[i]);
-			for(z=0;z<lottery[i];z++)
-			{
-				printf("\t::%d::",ticket[i][z]);
-			}       
-		}
-	}
-	
-	
-	int winner = (rand()%max_tick-1)+ 1;;
-    for(i =0;i<10;i++)
-        for(z=0;z<lottery[i];z++)
-            if(ticket[i][z]==winner)
-                q=i;
-    printf("\n\n-------------------------------------");
-    printf("<<<<  Winner: %d   >>>>",winner);
-    printf("-------------------------------------\n\n");
-	
-	
-	if ((brust[q] > 0))  
-	{
-        temp[q] = process[q];
-        brust[q] -= quantom;
+	//sorting burst time, priority and process number in ascending order using selection sort
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(priority[j]<priority[pos])
+                pos=j;
+        }
+ 
+        temp=process[i];
+        process[i]=process[pos];
+        process[pos]=temp;
+ 
+        temp=brust[i];
+        brust[i]=brust[pos];
+        brust[pos]=temp;
+ 
+        temp=priority[i];
+        priority[i]=priority[pos];
+        priority[pos]=temp;
         
-        if (brust[q] > 0) {
-            time += quantom;
-        } else {
-            time += (brust[q] + quantom);
-            }
-		
-		if (brust[q] < 0) {
-                brust[q] = 0;
-            }
-	    printf("Process That Are Running Is: %c",temp[q]);
-	    printf("\n(Total Time << Remaining Brust time << process ):  ( %d  <<  %d  <<  %c )\n\n",time,brust[q],temp[q]);
-	
+        if(brust[i]<0)
+		{
+			flag = 1;
+		}
     }
     
-    else if ((brust[q] > 0) && (time < arrival[q])) {
-        temp[q] = process[p];
-        if (brust[q] > 0) {
-                time += quantom;
-        } else {
-                time += (brust[q] + quantom);
+    if(flag==1)
+    {
+    	printf("\n\n:::  Invalid Time Entered ::: \n");
+    	exit(0);
+	}
+    
+    printf("\n |Priority |  Process   |  Brust |");
+	
+	for(i=0;i<n;i++)
+	{
+		printf("\n |   %d \t   |    %c    \t|   %d    |",priority[i],process[i],brust[i]);
+		tbt = tbt + brust[i];
+	}
+	
+	printf("\n\n:::::::::   Quantom time is 1 Sec   ::::::::: \n\n");
+	
+	//assign one or more lottery numbers to each process
+	int p=1,m_ticket=0;
+	printf("\n\n\nPriority  process  Brust  Lottery   Tickets");
+	for(i=0;i<n;i++)
+	{
+		lottery[i] = (brust[i]/quantom) + (n-priority[i]);
+		for (z=0;z<lottery[i];z++) 
+		{
+            ticket[i][z] = p++;
+            m_ticket = p;
         }
-        
-        printf("Process That Are Runnring Is: %c",temp[q]);
-	    printf("\n%d \t%d \t%c\n\n",time,brust[q],temp[q]);
-    }
-    pss++;
-}
-
-     printf("\n\n::::::::  Total Time Taken by CPU to Run All The Process: %d Sec  ::::::::\n\n\n",time);
-     
+      			
+		printf("\n  %d\t    %c\t    %d \t    %d",priority[i],process[i],brust[i],lottery[i]);
+		for(z=0;z<lottery[i];z++)
+		{
+			printf("\t");
+			printf("   ::%d::",ticket[i][z]);
+		}   
+	}
+		
+	
 }
